cff2641940fa21b2bec9c8d4caec14d07b842d57
Sergei.Petrushin <sjabrail@gmail.com>
2023-06-05 14:08:35 +0530

    program is done

---
+++ hw_0309_task64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введите натуральное цисло: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void Massive(int num)
+{
+    if(num == 1) Console.Write("1, ");
+    else
+    {
+        Console.Write($"{num}, ");
+        Massive(num - 1);
+    }
+}
+
+Massive(n);
