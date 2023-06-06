1a734963a722cd7207e9df16ab4b12c4e4e21427
Sergei.Petrushin <sjabrail@gmail.com>
2023-06-06 18:25:48 +0530

    program is done

---
+++ hw_0309_task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите натуральное цисло: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное цисло: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m > n)
+{
+    (m, n) = (n, m);
+}
+
+
+int SumElements(int start, int end)
+{
+    if(end == start)
+        return start;
+    else
+    {
+        return start + SumElements(start +1, end);
+    }
+}
+
+Console.WriteLine(SumElements(m, n));
